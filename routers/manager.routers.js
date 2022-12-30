5b3d27c0817b30a2c2b4b1da20ef93d5871deb73
thanh2212 <20020472@vnu.edu.vn>
2022-12-30 22:17:45 +0700

    done

--- routers/manager.routers.js
+++ routers/manager.routers.js
@@ -5,10 +5,10 @@ const router = require('express').Router();
 router.get('/list_user', manager.getAllUser);
 router.get('/portfolio_product', manager.getPortFolioProduct);
 router.post('/delete_account', manager.deleteAccount);
-router.post('/list_all_product', manager.staticAllProduct);
+router.get('/list_all_product', manager.staticAllProduct);
 
 router.post('/create_account', manager.createAccount); //
 router.get('/all_product', manager.getAllProduct);
-router.get('/get_profile_by_username?', manager.getProfileByUsername);
+router.get('/get_profile_by_username', manager.getProfileByUsername);
 
 module.exports = router;
